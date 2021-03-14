8843e0fc9aa3add0e4ab71f669a9beced4ef2ce8
Vladimir Bulanov <horobirus@gmail.com>
2021-03-14 17:11:59 +0300

    fix eslintrc

--- src/index.js
+++ src/index.js
@@ -1,78 +1,77 @@
 // const data = require('./data')
-const testData = require('./test-data')
-const getCalculation = require('./calculation')
-const getDividedDiffTable = require('./get-table')
-const getNodes = require('./get-nodes')
+const testData = require("./test-data");
+const getCalculation = require("./calculation");
+const getDividedDiffTable = require("./get-table");
+const getNodes = require("./get-nodes");
 
-const { X_VALUES, Y_VALUES, INTERPOLATION_X } = testData
+const { X_VALUES, Y_VALUES, INTERPOLATION_X } = testData;
 
 // вывод начальных данных
 const printInitial = (sortedX, yValues, n, interpolationX) => {
   console.log(
-    'Исходные данные: \n ',
-    'Значения X: \n',
-    sortedX.join(', '),
-    '\n Значения Y: \n',
-    yValues.join(', '),
-    '\n Степень полинома n: \n',
+    "Исходные данные: \n ",
+    "Значения X: \n",
+    sortedX.join(", "),
+    "\n Значения Y: \n",
+    yValues.join(", "),
+    "\n Степень полинома n: \n",
     n,
-    '\n Значение X: \n',
+    "\n Значение X: \n",
     interpolationX
-  )
-}
+  );
+};
 
 const interpolate = (xValues, yValues, n, interpolationX) => {
   const isValidTypes =
     Array.isArray(xValues) &&
     Array.isArray(yValues) &&
-    typeof n === 'number' &&
-    typeof interpolationX === 'number'
+    typeof n === "number" &&
+    typeof interpolationX === "number";
 
   if (!isValidTypes) {
-    throw new Error('Types are invalid, mission abort!!!')
-    return null
+    throw new Error("Types are invalid, mission abort!!!");
   }
 
   const isValidData =
     xValues.length === yValues.length &&
-    xValues.filter((val) => typeof val === 'number').length ===
+    xValues.filter((val) => typeof val === "number").length ===
       xValues.length &&
-    yValues.filter((val) => typeof val === 'number').length === yValues.length
+    yValues.filter((val) => typeof val === "number").length === yValues.length;
 
   if (!isValidData) {
-    throw new Error('Data is invalid, mission abort!!!')
-    return null
+    throw new Error("Data is invalid, mission abort!!!");
+    return null;
   }
 
-  const sortedX = xValues.sort((a, b) => a - b)
+  const sortedX = xValues.sort((a, b) => a - b);
 
-  printInitial(sortedX, yValues, n, interpolationX)
+  printInitial(sortedX, yValues, n, interpolationX);
 
-  const nodes = getNodes(sortedX, yValues, n, interpolationX)
+  const nodes = getNodes(sortedX, yValues, n, interpolationX);
 
   if (!nodes) {
-    return null
+    return null;
   }
 
-  console.log('Nodes: \n', nodes)
+  console.log("Nodes: \n", nodes);
 
-  const table = getDividedDiffTable(n, nodes.xNodes, nodes.yNodes)
+  const table = getDividedDiffTable(n, nodes.xNodes, nodes.yNodes);
 
-  console.log('Divided diff table: \n', table)
+  console.log("Divided diff table: \n", table);
 
-  return getCalculation(table, nodes.xNodes)
-}
+  return getCalculation(table, nodes.xNodes);
+};
 
 // Вывод результатов расчетов для каждого n
 for (let i = 0; i < 5; i++) {
   console.log(
     `Результат при n=${i}: \n`,
     interpolate(X_VALUES, Y_VALUES, i, INTERPOLATION_X),
-    '\n\n\n'
-  )
+    "\n\n\n"
+  );
 }
 
 console.log(
-  'Результат при обратной интерполяции: \n',
+  "Результат при обратной интерполяции: \n",
   interpolate(Y_VALUES, X_VALUES, 5, 0)
-)
+);
